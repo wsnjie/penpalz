9cb48c94e569d979de9fc4f07bc1722429e9caa0
Will <megapocketninja@gmail.com>
2019-03-30 00:53:36 -0400

    Started models definitions

--- penpalz_app/models.py
+++ penpalz_app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.CharField(max_length=255)
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.firstname
+
+
+class Lang(models.Model):
+    name = models.CharField(max_length=100)
+    avglvl = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Message(models.Model):
+    sent = models.ForeignKey(User, related_name="from_user", on_delete=models.CASCADE)
+    rec = models.ForeignKey(User, related_name="to_user", on_delete=models.CASCADE)
+    language = models.ForeignKey(
+        Lang, related_name="in_language", on_delete=models.DO_NOTHING
+    )
+    content = models.TextField()
+
+
+class Prof(models.Model):
+    user = models.ForeignKey(
+        User, related_name="prof_of_user", on_delete=models.CASCADE
+    )
+    language = models.ForeignKey(
+        Lang, related_name="prof_of_lang", on_delete=models.CASCADE
+    )
+    level = models.IntegerField(max_length=2)
